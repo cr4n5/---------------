9a3f75e1a9f844717dbee81732bd224656b5357e
sd0ric4 <1286518974@qq.com>
2024-10-13 18:28:06 +0800

    Refactor frontend router: Add route guard for login page

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import LoginView from '@/views/LoginView.vue'
+import { useUserStore } from '@/stores/user' // 导入用户存储
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -12,9 +14,6 @@ const router = createRouter({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
     },
     {
@@ -38,6 +37,18 @@ const router = createRouter({
       component: LoginView
     }
   ]
+})
+
+// 添加路由守卫
+router.beforeEach((to, from, next) => {
+  const userStore = useUserStore()
+  const isLoggedIn = !!userStore.token
+
+  if (isLoggedIn && to.name === 'login') {
+    next({ name: 'home' })
+  } else {
+    next()
+  }
 })
 
 export default router
